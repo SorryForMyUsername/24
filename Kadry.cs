8ebdc9a81ecfee7adc40e700ce5802e5205016d6
Ilya <119899583+SorryForMyUsername@users.noreply.github.com>
2025-02-20 11:11:06 +0200

    Add files via upload

---
+++ Kadry.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24_3
+{
+    internal class Kadry
+    {
+        /// <summary>
+        /// Поле Фамилия
+        /// </summary>
+        string fam;
+        /// <summary>
+        /// Поле Возраст
+        /// </summary>
+        int age;
+        /// <summary>
+        /// Поле Должность
+        /// </summary>
+        string dol;
+        /// <summary>
+        /// Поле Стаж
+        /// </summary>
+        int staj;
+
+        /// <summary>
+        /// Свойство Фамилия
+        /// </summary>
+        public string Fam {
+            get { return fam; }
+            set { fam = value; }
+        }
+        /// <summary>
+        /// Свойство Возраст
+        /// </summary>
+        public int Age {
+            get { return age; }
+            set
+            {
+                age = value < 16 
+                    ? 16 
+                    : value > 60 ? 60 : value;
+            }
+        }
+        /// <summary>
+        /// Свойство Должность
+        /// </summary>
+        public string Dol {
+            get { return  dol; }
+            set { dol = value; }
+        }
+        /// <summary>
+        /// Свойство Стаж
+        /// </summary>
+        public int Staj {
+            get { return staj; }
+            set
+            {
+                staj = value < 0
+                    ? 0
+                    : value > 45 ? 45 : value;
+            }
+        }
+
+        /// <summary>
+        /// Конструктор по умолчанию
+        /// </summary>
+        public Kadry() { }
+
+        /// <summary>
+        /// Конструктор с четырьмя параметрами
+        /// </summary>
+        /// <param name="fam">Фамилия</param>
+        /// <param name="age">Возраст</param>
+        /// <param name="dol">Должность</param>
+        /// <param name="staj">Стаж</param>
+        public Kadry(string fam, int age, string dol, int staj)
+        {
+            Fam = fam;
+            Age = age;
+            Dol = dol;
+            Staj = staj;
+        }
+
+        public override string ToString()
+        {
+            return $"Фамилия: {Fam}\tВозраст: {Age}\tДолжность: {Dol}\tСтаж: {Staj}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Kadry kadr1 = new Kadry();
+            kadr1.Fam = "Фамилия";
+            kadr1.Age = 30;
+            kadr1.Dol = "Должность";
+            kadr1.Staj = 7;
+            Console.WriteLine(kadr1);
+            Console.WriteLine();
+
+            Kadry kadr2 = new Kadry("Фам", 62, "Дол", 46);
+            Console.WriteLine(kadr2);
+            Console.WriteLine();
+
+            Console.Write("Фамилия: ");
+            string fam = Console.ReadLine();
+            Console.Write("Возраст: ");
+            int age = int.Parse(Console.ReadLine());
+            Console.Write("Должность: ");
+            string dol = Console.ReadLine();
+            Console.Write("Стаж: ");
+            int staj = int.Parse(Console.ReadLine());
+            Kadry kadr3 = new Kadry(fam, age, dol, staj);
+            Console.WriteLine(kadr3);
+            Console.WriteLine();
+
+            Console.ReadKey(true);
+        }
+    }
+}
